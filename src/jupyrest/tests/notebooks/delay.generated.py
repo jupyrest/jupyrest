e427464b618bf7f4adaf77f3152bc6ef0554cf0a
koek67 <krishnan.koushik@gmail.com>
2024-04-06 01:59:23 -0400

    update notebooks

---
+++ src/jupyrest/tests/notebooks/delay.generated.py
@@ -0,0 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.16.1
+#   kernelspec:
+#     display_name: .venv
+#     language: python
+#     name: python3
+# ---
+
+# %% tags=["parameters"]
+delay_seconds = 5
+
+# %%
+import asyncio
+await asyncio.sleep(delay_seconds)
