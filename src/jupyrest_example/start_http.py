cc566e3c29cad674cc8a2fcb39ffed0762719f98
koek67 <krishnan.koushik@gmail.com>
2024-05-16 18:30:46 -0400

    adding docs

--- src/jupyrest_example/start_http.py
+++ src/jupyrest_example/start_http.py
@@ -2,6 +2,7 @@ from jupyrest.http.asgi import create_asgi_app
 import uvicorn
 from jupyrest.infra.in_memory.builder import InMemoryApplicationBuilder
 from jupyrest_example import Portfolio, notebooks_dir
+import sys
 
 # Build our Jupyrest Application, here we are using the in-memory configuration
 deps = InMemoryApplicationBuilder(
@@ -10,7 +11,15 @@ deps = InMemoryApplicationBuilder(
 
 # Generate a FastAPI application
 app = create_asgi_app(deps=deps)
-
+default_port = 5051
 # start the server
 if __name__ == "__main__":
-    uvicorn.run(app, port=5051)
+    if len(sys.argv) < 2:
+        port = default_port
+    else:
+        try:
+            port = int(sys.argv[1])
+        except:
+            print("Unable to parse port number, using default port: ", default_port)
+            port = default_port
+    uvicorn.run(app, port=port)
